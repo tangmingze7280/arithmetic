f33432eca78a873c9f9f95b15e92e4a273fe6686
Administrator <1>
2020-08-18 23:24:45 +0800

    1

---
+++ src/main/java/recoverlinked/Main.java
@@ -0,0 +1,32 @@
+package recoverlinked;
+
+/**
+ * @author tmz
+ * @since 08/18
+ * @version 1.0
+ * @deprecated  <p>单向链表进行反单向链表</p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.setData(1);
+        node.setNext(new Node(2,new Node(3,null)));
+//        node.print();
+        recover(node);
+    }
+    private static Node recover(Node node){
+        if (node==null||null==node.getNext()){
+            return node;
+        }
+        Node pre =null;
+        Node curr=node;
+        Node next=null;
+        while (null!=curr.getNext()){
+            next = curr.getNext();
+            curr.setNext(pre);//将前置节点被当前指针next选中
+            pre = curr;//前置指针移动到当前节点
+            curr = next;//下一个
+        }
+        return pre;
+    }
+}
---
+++ src/main/java/recoverlinked/Node.java
@@ -0,0 +1,48 @@
+package recoverlinked;
+
+import lombok.Data;
+
+
+public class Node {
+    Object data;
+    Node next;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(Object data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node() {
+    }
+    public void print(){
+        Node cur=this.next;
+        while(cur!=null){
+            System.out.printf(cur.toString());
+            cur = this.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
+}
